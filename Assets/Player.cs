62eaed7818c42db689fca526c659f86194fb9a7f
Hrishi <hrishikeshmane87933@gmail.com>
2018-09-01 10:48:46 +0530

    project pushed

---
+++ Assets/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 30f;
+	public float mapWidth = 1.8f;
+
+	private Rigidbody2D rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate ()
+	{
+		float x = CrossPlatformInputManager.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
+		//rb.MovePosition (rb.position + Vector2.right * x);
+		Vector2 newPosition = rb.position + Vector2.right * x;
+
+		newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth);
+
+		rb.MovePosition(newPosition);
+
+	}
+
+	void OnCollisionEnter2D ()
+	{
+		FindObjectOfType<GameManager>().EndGame();
+	}
+
+}
